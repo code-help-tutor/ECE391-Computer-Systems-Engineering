f17be68cae70d761c4586253ca3892cc4a654d78
Code Help Tutor <1823890830@qq.com>
2024-01-16 05:51:57 +0000

    WeChat cstutorcs ECE391 Computer Systems Engineering

---
+++ ECE391-MP3/syscalls/ece391testprint.c
@@ -0,0 +1,16 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+#include <stdint.h>
+
+#include "ece391support.h"
+#include "ece391syscall.h"
+
+int main ()
+{
+
+    ece391_fdputs (1, (uint8_t*)"Hello, if this ran, the program was correct. Yay!\n");
+
+    return 0;
+}
+
